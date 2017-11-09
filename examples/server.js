325831c82550f2b6a83c7cae38aaa0d6d3dcd943
Evan Sosenko <evan.sosenko@meltwater.com>
2017-11-09 12:19:00 -0800

    Clean up example

--- examples/server.js
+++ examples/server.js
@@ -1,15 +1,16 @@
 import { createContainer, Lifetime } from 'awilix'
 import Koa from 'koa'
 import Router from 'koa-router'
+import koaMount from 'koa-mount'
 import { createHealthMonitor, healthLogging } from '@meltwater/mlabs-health'
 
 import { createServer, koaHealthy } from '../lib'
 
 const { SCOPED, SINGLETON } = Lifetime
 
-const createHealth = container => {
-  return createHealthMonitor({puppies: () => container.resolve('puppies')})
-}
+const createHealth = container => createHealthMonitor({
+  puppies: () => container.resolve('puppies')
+})
 
 const createStart = ({log, healthMonitor}) => async () => {
   healthLogging({log, healthMonitor})
@@ -22,9 +23,9 @@ const createApp = ({log} = {}) => {
   const app = new Koa()
   const router = new Router()
 
-  router.get('/api/v1/health', koaHealthy())
-  app.use(router.routes())
-  app.use(router.allowedMethods())
+  router.get('/health', koaHealthy())
+  app.use(koaMount('/api/v1', router.routes()))
+  app.use(koaMount('/api/v1', router.allowedMethods()))
 
   return app
 }
@@ -37,25 +38,25 @@ const createPuppies = ({log, reqId} = {}) => {
   return {health}
 }
 
+const createDependencies = log => ({config}) => {
+  const container = createContainer()
+
+  container.registerValue({log})
+  container.registerValue({reqId: null})
+  container.registerValue({createHealthMonitor: createHealth})
+  container.registerFunction({start: [createStart, {lifetime: SINGLETON}]})
+  container.registerFunction({stop: [createStop, {lifetime: SINGLETON}]})
+  container.registerFunction({app: [createApp, {lifetime: SINGLETON}]})
+
+  container.registerFunction({puppies: [createPuppies, {lifetime: SCOPED}]})
+
+  return container
+}
+
 export default ({log}) => (port = 9000) => {
-  const createDependencies = ({config}) => {
-    const container = createContainer()
-
-    container.registerValue({log})
-    container.registerValue({reqId: null})
-    container.registerValue({createHealthMonitor: createHealth})
-    container.registerFunction({start: [createStart, {lifetime: SINGLETON}]})
-    container.registerFunction({stop: [createStop, {lifetime: SINGLETON}]})
-    container.registerFunction({app: [createApp, {lifetime: SINGLETON}]})
-
-    container.registerFunction({puppies: [createPuppies, {lifetime: SCOPED}]})
-
-    return container
-  }
-
   const { configFactory, run } = createServer({
     configPath: __dirname,
-    createDependencies
+    createDependencies: createDependencies(log)
   })
 
   configFactory.addOverride({port})
